fdaa778d9c89425976c40598bda06396a24e674c
abeonweb <bmospecific@yahoo.com>
2022-10-17 12:19:10 +0100

    comment out axios import

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Formik, Form, Field, ErrorMessage } from "formik"
-import axios from "axios"
+// import axios from "axios"
 import * as Yup from "yup"
 // import "animate.css"
 import "../css/Form.css"
