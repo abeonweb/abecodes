1e8e27745238f9c2718f8ebfaa7126e4b8c9f9a5
abeonweb <bmospecific@yahoo.com>
2023-11-18 07:37:25 +0100

    import css module

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,26 +1,28 @@
+import styles from "../css/Project.module.css";
+
 export default function Project(props) {
 
     const techStack = props.project.stack.map((tech, index) => (
-        <li key={index} className="project-stack">
+        <li key={index} className={styles["project-stack"]}>
             {tech}
         </li>
     ))
     return (
-        <article key={props.project.id} className="project">
-            <h2 className="project-title">{props.project.title}</h2>
-            <div className="project-flex-container">
-                <div className="project-info-container">
-                    <ul className="stack-container">
+        <article key={props.project.id} className={styles["project"]}>
+            <h2 className={styles["project-title"]}>{props.project.title}</h2>
+            <div className={styles["project-info-image-container"]}>
+                <div className={styles["project-info-container"]}>
+                    <ul className={styles["stack-container"]}>
                         {techStack}
                     </ul>
-                    <p className="project-description">{props.project.description}</p>
-                    <div className="project-links-container">
-                        {props.project.showSite && <a href={props.project.site} target="_blank" rel="noopener noreferrer" className="project-links">Site</a>}
-                        {props.project.showGithub && <a href={props.project.github} target="_blank" rel="noopener noreferrer" className="project-links">Github</a>}
+                    <p className={styles["project-description"]}>{props.project.description}</p>
+                    <div className={styles["project-links-container"]}>
+                        {props.project.showSite && <a href={props.project.site} target="_blank" rel="noopener noreferrer" className={styles["project-links"]}>Site</a>}
+                        {props.project.showGithub && <a href={props.project.github} target="_blank" rel="noopener noreferrer" className={styles["project-links"]}>Github</a>}
                     </div>
                 </div>
-                <div className="project-image-container">
-                    <img src={require("../images/" + props.project.image + ".webp")} alt={props.project.alt} loading="lazy" className="project-image" />
+                <div className={styles["project-image-container"]}>
+                    <img src={require("../images/" + props.project.image + ".webp")} alt={props.project.alt} loading="lazy" className={styles["project-image"]} />
                 </div>
             </div>
         </article>
