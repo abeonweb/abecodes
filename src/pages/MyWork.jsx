5729b8a29f89de19ee1a6f18a956aa67bb007a78
abeonweb <bmospecific@yahoo.com>
2023-11-18 07:41:09 +0100

    update to component to match css module format

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -1,11 +1,12 @@
+import styles from "../css/MyWork.module.css";
 import Project from "../components/Project";
 import { projects } from "../data";
 
 export default function MyWork() {
   const allProjects = projects.map((project, index) => <Project key={index} project={project} />)
   return (
-    <section id="work" className="section portfolio">
-      <h1 className="title page-title portfolio-page-title">My work</h1>
+    <section id="work" className={`section ${styles["portfolio"]}`}>
+      <h1 className={`title ${styles["portfolio-page-title"]}`}>My work</h1>
       {allProjects}
     </section>
   );
