4b99983dfab64f21698dac1a59bd7619572e3c23
abeonweb <bmospecific@yahoo.com>
2026-01-19 13:03:39 +0100

    add blog

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import MyWork from "./pages/MyWork";
 import About from "./pages/About";
-// import Blog from "./pages/Blog";
-// import BlogPost from "./pages/BlogPost";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 import Success from "./pages/Success"
 import NotFound from "./pages/NotFound";
 import SharedLayout from "./components/SharedLayout";
@@ -16,8 +16,8 @@ function App() {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="work" element={<MyWork />} />
-        {/* <Route path="blog" element={<Blog />} />
-        <Route path="blog/:slug" element={<BlogPost />} /> */}
+        <Route path="blog" element={<Blog />} />
+        <Route path="blog/:slug" element={<BlogPost />} />
         <Route path="about" element={<About />} />
         <Route path="success" element={<Success />} />
         <Route path="*" element={<NotFound />} />
