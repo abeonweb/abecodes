072b51183f9e707b358e65fca15ca4783de1aa0f
abeonweb <bmospecific@yahoo.com>
2026-01-19 12:59:47 +0100

    update to remove unused components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import MyWork from "./pages/MyWork";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
+// import Blog from "./pages/Blog";
+// import BlogPost from "./pages/BlogPost";
 import Success from "./pages/Success"
 import NotFound from "./pages/NotFound";
 import SharedLayout from "./components/SharedLayout";
